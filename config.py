fbc9a5661bb47aad79f6795e89fd03dedb1488e8
InfiniteJack1 <vipin275869@gmail.com>
2026-04-07 10:26:00 +0530

    Update config.py

--- config.py
+++ config.py
@@ -33,7 +33,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/vipin-kumar735/Andaz",
+    "https://github.com/InfiniteJack1/Army",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
 GIT_TOKEN = getenv(
